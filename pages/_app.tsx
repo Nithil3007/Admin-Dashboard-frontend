a3392459b88e29cb68ea55917577fb00080f45e7
Nithil Eshwar <102175607+Nithil3007@users.noreply.github.com>
2025-10-03 00:32:52 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import 'antd/dist/reset.css';
-import { configureAmplify } from '@/lib/amplify-config';
-import { AuthProvider } from '@/contexts/AuthContext';
+import { configureAmplify } from '../lib/amplify-config';
+import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
